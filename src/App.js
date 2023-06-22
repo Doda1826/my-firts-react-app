55af1f5311b700eda0bbec0b846474f9bccca00f
pikachububble <darwinjverasteguis2002@gmail.com>
2023-06-22 10:12:33 -0500

    routing the wed app to be connected with the another documents

--- src/App.js
+++ src/App.js
@@ -1,27 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import { createBrowserRouter } from 'react-router-dom';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 const router = createBrowserRouter( /* application routes are defined here */ );
-
-function App() {
+ 
+export default function App () {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <RouterProvider router={ router } />
   );
 }
-
-export default App;
