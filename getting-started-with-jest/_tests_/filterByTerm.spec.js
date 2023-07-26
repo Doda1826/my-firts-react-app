aee01b89564ce72a02a3406cf2701a45577bad5e
pikachububble <darwinjverasteguis2002@gmail.com>
2023-07-26 12:02:32 -0500

    files added and some modified for test jest

--- getting-started-with-jest/_tests_/filterByTerm.spec.js
+++ getting-started-with-jest/_tests_/filterByTerm.spec.js
@@ -1,13 +1,24 @@
+const filterByTerm = require("../src/filterByTerm");
+
 describe("Filter function", () => {
     test("it should filter by a search term (link)", () =>  {
         const input = [
             {id: 1, url: "https://www.ulr1.dev"},
             {id: 2, url: "https://www.ulr2.dev"},
-            {id: 3, url: "https://www.ulr3.dev"}
+            {id: 3, url: "https://www.link3.dev"}
         ]
 
         const output = [{id: 3, url: "https://www.link3.dev"}]
 
         expect(filterByTerm(input, "link")).toEqual(output)
+
+        expect(filterByTerm(input, "LINK")).toEqual(output) //new test
     })
 })
+
+function filterByTerm(inputArr, searchTerm) {
+    const regex = new RegExp(searchTerm, "i")
+    return inputArr.filter(function(arrayElement){
+        return arrayElement.url.match(regex)
+    })
+}
