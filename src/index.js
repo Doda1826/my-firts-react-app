f5000e112356f9d88863e8f94628a0a8a1fd4b5c
pikachububble <darwinjverasteguis2002@gmail.com>
2023-11-05 13:21:04 -0500

    added

--- src/index.js
+++ src/index.js
@@ -4,13 +4,20 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+class MyComponent extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+  render() {
+    return (
+      <App>
+        
+      </App>
+    );
+  }
+}
 
+ReactDOM.render(<MyComponent/>, document.getElementById("Root")); 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
